3ed2f149bc21dc6255400e254bcc76f95ffe0c49
anumohan208 <154403264+anumohan208@users.noreply.github.com>
2024-01-28 21:17:17 -0600

    Graded Assignment 1_Part 3 completed

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,88 +3,84 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName="" ;
+let candidateName = "";
 
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question ="Who was the first American woman in space? ";
-let correctAnswer="Sally Ride";
-let candidateAnswer="";
+let question = "Who was the first American woman in space? ";
+let correctAnswer = "Sally Ride";
+let candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
-let questions=["Who was the first American woman in space? ",
-"True or false: 5 kilometer == 5000 meters? ",
-"(5 + 3)/2 * 10 = ? ",
-"Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
-"What is the minimum crew size for the ISS? "];
-let correctAnswers=["Sally Ride","true","40","Trajectory","3"];
-let candidateAnswers=[];
+let questions =
+  ["Who was the first American woman in space? ",
+    "True or false: 5 kilometer == 5000 meters? ",
+    "(5 + 3)/2 * 10 = ? ",
+    "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
+    "What is the minimum crew size for the ISS? "];
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
+let candidateAnswers = [];
 
 
 function askForName() {
-  // TODO 1.1b: Ask for candidate's name //
-candidateName  = input.question("Enter Your Name:")
+  // TODO 1.1b: Ask for candidate's name 
+  candidateName = input.question("Enter Your Name:")
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-//candidateAnswer=input.question(question);
-for(var i=0;i<5;i++){
-candidateAnswers[i]=input.question(questions[i]);
-if(candidateAnswers[i].toLowerCase()==correctAnswers[i].toLowerCase()){
-console.log("Correct Answer!\n")
-}else{
-  console.log("Your Answer is Wrong\n");
-}
-}
+  //candidateAnswer=input.question(question);
+  for (var i = 0; i < 5; i++) {
+    candidateAnswers[i] = input.question(questions[i]);
+  }
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
   /*
-  if(candidateAnswer.toLowerCase()==correctAnswer.toLocaleLowerCase())
+  if(candidateAnswer.toLowerCase()==correctAnswer.toLowerCase())
   {
     console.log(candidateName+"Your Answer is Correct");
   } 
   else{
     console.log(candidateName+"Your Answer is Wrong");
   }*/
-let mark=0;
-for(var i=0;i<5;i++){
- 
-if(candidateAnswers[i].toLowerCase()==correctAnswers[i].toLowerCase()){
+  let mark = 0;
+  for (let i = 0; i < 5; i++) {
 
- console.log("Your Answer for the question",'"',questions[i],'" :',candidateAnswers[i]);
- console.log("Correct Answer:",correctAnswers[i]);
-  mark++;
-}
-else{
- console.log("Your Answer for the question",'"',questions[i],'" :',candidateAnswers[i]);
- console.log("Correct Answer:",correctAnswers[i]);
-}
+    if (candidateAnswers[i].toLowerCase() == correctAnswers[i].toLowerCase()) {
+
+      console.log("Your Answer for the question", '"', questions[i], '" :', candidateAnswers[i]);
+      console.log("Correct Answer:", correctAnswers[i]);
+      mark++;
+    }
+    else {
+      console.log("\nYour Answer for the question", '"', questions[i], '" :', candidateAnswers[i]);
+      console.log("Correct Answer:", correctAnswers[i]);
+    }
 
 
   }
-  let grade=(mark/5)*100;  //TODO 3.2 use this variable to calculate the candidates score.
-   return grade;
+  let grade = (mark / 5) * 100;  //TODO 3.2 use this variable to calculate the candidates score.
+
+  return grade;
 }
 
 function runProgram() {
   askForName();
   // TODO 1.1c: Greet candidate using their name //
-   console.log("Hello",candidateName,"\n");
+  console.log("Hello", candidateName, "\n");
   askQuestion();
 
- let totalGrade=gradeQuiz(this.candidateAnswers);
- console.log("\nTotal Grade",totalGrade+"%","\n");
-  if(totalGrade>=80)
-  {
+  let totalGrade = gradeQuiz(this.candidateAnswers);
+
+  console.log("\nTotal Grade", totalGrade + "%");
+  if (totalGrade >= 80)
     console.log("Status: PASS");
-  }
-  else{
+  else
     console.log("Status: FAILED");
-  }
+
 }
 
 // ----------- Don't write any code or change any code below this line ---------- //
